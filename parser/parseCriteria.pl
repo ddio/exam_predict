a563bffcd5a6f6aef3c65acb97c8db91e92460c3
ddio <ddioibb@gmail.com>
2014-02-17 23:32:30 +0800

    clean up parser

--- parser/parseCriteria.pl
+++ parser/parseCriteria.pl
@@ -20,7 +20,7 @@ $suf = ');';
 
 %departments = ();
 
-$dbh = DBI->connect( "dbi:SQLite:dbname=../../db/examPredict.sqlite" ) || die "Cannot connect: $DBI::errstr";
+$dbh = DBI->connect( "dbi:SQLite:dbname=../../../db/2014examPredict.sqlite" ) || die "Cannot connect: $DBI::errstr";
 $scQuery = $dbh->prepare( 'select * from departments' );
 $scQuery->execute();
 
@@ -39,8 +39,23 @@ for( $ai = 0; $ai <= $#ARGV; $ai++ ) {
 	
 	$schoolName = basename($ARGV[$ai]);
 	$schoolName =~ s/.csv$//;
+	
+	print STDERR "==============\n\t$schoolName\n";
 
 	open FILE, $ARGV[$ai];
+
+	$line = <FILE>;
+	chomp $line;
+	$line =~ s/^([^ (]*)[ (].*$/$1/;
+	$schoolName = $line;
+	$schoolName =~ s/台/臺/;
+#	@meta = split / /, $line;
+#	@meta = split /\(/, $meta[0];
+#	$schoolName = $meta[0];
+
+	print STDERR "\t$schoolName\n===============\n";
+
+	<FILE>;
 
 	while(<FILE>) {
 		chomp;
@@ -51,16 +66,16 @@ for( $ai = 0; $ai <= $#ARGV; $ai++ ) {
 		if ( /^,*$/ ) { next; }
 		@column = split /,/;
 
-		if ( $column[1] =~ /100/ ) { next; }
+		if ( $column[1] =~ /10[01]/ ) { next; }
 
 		if ( $column[1] =~ /$ *^/ || $column[2] =~ /$ *^/ ) { 
 #			print STDERR "skipping empty $schoolName/$column[0]: $column[1]\n";
 			next; 
 		}
 
-		if ( $column[0] =~ /^(.*)（(男|女)）/ ) {
+		if ( $column[0] =~ /^(.*)\((男|女)\)/ ) {
 			$dep = $1;
-#			print STDERR "dectect sex $schoolName $dep\n";
+			print STDERR "dectect sex $schoolName $dep\n";
 			$column[0] = "1,'$dep'" if $2 =~ /男/;
 			$column[0] = "2,'$dep'" if $2 =~ /女/;
 		} else {
@@ -69,8 +84,9 @@ for( $ai = 0; $ai <= $#ARGV; $ai++ ) {
 		}
 		$column[0] =~ /'(.*)'/;
 		$dep = $schoolName.$1;
+		$dep =~ s/台/臺/;
 		unless ( exists( $departments{$dep} ) ) {
-#			print STDERR "no such dept $dep\n";
+			print STDERR "no such dept $dep\n";
 			next;
 		}
 		$column[0] =~ s/'.*'/$departments{$dep}/;
@@ -103,11 +119,12 @@ for( $ai = 0; $ai <= $#ARGV; $ai++ ) {
 			}
 		}
 
-		$column[21] =~ s/-|－|—|^0$|不足額/NULL/; #超額篩選
+		$column[21] =~ s/^(-|－|—|0|不足額|無超額|)$/NULL/; #超額篩選
 		$column[21] =~ s/<|u//;	#超額篩選
+		$column[22] =~ s/^$/0/;	#二階段錄取人數
 		$column[23] =~ s/^$/0/;	#備取人數
-		$column[24] =~ s/備.*(\d+)/$1/;	# 分發最低標準
-		$column[24] =~ s/正取|IE取|無/0/;
+		$column[24] =~ s/.*(\d+).*/$1/;	# 分發最低標準
+		$column[24] =~ s/正取|IE取|無|備|-|^$/0/;
 
 		$column[25] =~ s/V/1/;	#toeic
 		$column[25] =~ s/^$/0/;
